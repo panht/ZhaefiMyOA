e9cff46a11ed6456cabc375626ba4fcc2b42fca9
worldask <worldask@gmail.com>
2013-05-04 23:23:57 +0800

    修改显示文字

---
+++ webroot/general/crm/modules/Account/care/EditView/EditExtend.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function(){
+	var clickEvent = jQuery("#contact_id_text").attr("onclick");
+	jQuery("#contact_id_text").attr("onclick","");
+	clickEvent = clickEvent.replace("selRefSingle","selRefContact");
+	jQuery("#contact_id_text").click(function(){	
+		eval(clickEvent);
+	});
+	jQuery("#account_id").bind("propertychange",function(){
+		jQuery("#contact_id").val("");
+		jQuery("#contact_id_text").val("");
+	});
+});
+
+
+function selRefContact(to_id,to_value,view_id,entity){
+	var filter = "";
+	if(document.getElementById("account_id")){
+		var account_id = document.getElementById("account_id").value;
+		if(account_id == ""){
+			alert("请选择对应企业！");
+			return false;
+		}
+		filter = entity+".account_id ="+account_id;
+	}
+	var url = g_CRM_PATH+"/include/reference.php?to_id="+to_id+"&to_value="+to_value+"&view_id="+view_id+"&entity_name="+entity+"&filter="+filter;
+	openWindow(url,750,520,"","selRef");
+}
