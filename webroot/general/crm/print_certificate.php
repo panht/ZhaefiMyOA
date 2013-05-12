eb2645ea7ef2e85ae8f0d3a1e7e7875e3d6b36bc
worldask <worldask@gmail.com>
2013-05-12 14:56:21 +0800

    打印会员证

---
+++ webroot/general/crm/print_certificate.php
@@ -0,0 +1,49 @@
+<?php
+$connect = mysql_connect("127.0.0.1:3336", "root", "myoa888");
+if (!$connect){
+	die(mysql_error());
+}
+mysql_select_db("TD_OA", $connect);
+mysql_query("SET NAMES 'GBK'"); 
+
+$sql = "select account_name, field3, account_field8, account_birthday from crm_account where id = " . $_GET['id'] . " limit 1";
+$sqlResult = mysql_query($sql);
+while ($row = mysql_fetch_row($sqlResult)) {
+	$account_name = $row[0];
+	$leal_person = $row[1];
+	$certificate_no = $row[2];
+	$account_birthday = $row[3];
+}
+mysql_free_result($sqlResult);
+mysql_close($connect);
+?>
+<html>
+<head>
+<title>会员证>>打印预览</title>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312">
+<style>
+.print11 {
+    font-family: "黑体";
+    font-size: 16px;
+    font-weight: bold;
+}
+</style>
+</head>
+
+<body>
+<div style="position:absolute;left:-10;top:-10">
+<table border="0" width="100%" cellspacing="0" cellpadding="10"><tr><td>
+<img src='account_certificate.jpg' style='display:none'>
+<div style='position:absolute;left:10;top:710;width:760'>
+<table border="0" width="100%" cellspacing="0">
+	<tr><td align=center class=print09><?php echo $account_name?></td></tr>
+	<tr><td class=print10 align=center>法定代表人：<?php echo $leal_person?></td></tr>
+</table>
+</div>
+<div style='position:absolute;left:80;top:960' class=print11>证   号：<?php echo $certificate_no?></div>
+<div style='position:absolute;left:240;top:960' class=print11>入会日期：<?php echo date("Y年m月d日", $account_birthday)?></div>
+<div style='position:absolute;left:445;top:960' class=print11>发证日期：<?php echo date("Y年m月d日")?></div>
+</td></tr></table>
+</div>
+</body>
+</html>
