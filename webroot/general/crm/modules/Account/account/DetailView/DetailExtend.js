653e069b7906c62a852dca606f9321086f30c9b9
worldask <worldask@gmail.com>
2013-05-12 15:08:52 +0800

    添加打印会员证链接

---
+++ webroot/general/crm/modules/Account/account/DetailView/DetailExtend.js
@@ -0,0 +1,4 @@
+// 添加打印会员证
+var cert_pos = window.location.href.indexOf("&id=");
+var cert_id = window.location.href.substr(cert_pos + 4);
+jQuery("#sider .acts").prepend("<li><a href='' onclick=\"window.open('../../../../print_certificate.php?id=" + cert_id + "', '', '');\">打印会员证</a></li>");
