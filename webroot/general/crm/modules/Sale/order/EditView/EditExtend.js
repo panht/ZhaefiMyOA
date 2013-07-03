d38c8918195550554c9db65d3fbbc7671bc68ec2
worldask <worldask@gmail.com>
2013-07-03 17:37:00 +0800

    服务项目选择企业名称后，项目名称被清空的问题已经解决

--- webroot/general/crm/modules/Sale/order/EditView/EditExtend.js
+++ webroot/general/crm/modules/Sale/order/EditView/EditExtend.js
@@ -1,6 +1,10 @@
 jQuery(document).ready(function(){
-	var tmp_arr = new Array("opportunity_id", "opportunity_id_text", "contact_id", "contact_id_text", "quotation_id", "quotation_id_text");
+	//var tmp_arr = new Array("opportunity_id", "opportunity_id_text", "contact_id", "contact_id_text", "quotation_id", "quotation_id_text");
+	// account_id字段文本框值改变后，清后其它字段文本框数据
+	var tmp_arr = new Array("contact_id", "contact_id_text", "quotation_id", "quotation_id_text");
 	ClearField("account_id", "propertychange", tmp_arr);
+	
+	// 选择框依赖字段，选择企业联系人需先选择企业名称
 	DependField("contact_id_text", "click", "account_id", "crm_account_contact.account_id","account_id");
 	DependField("quotation_id_text", "click", "account_id", "crm_quotation.account_id","account_id");
 	//DependField("opportunity_id_text", "click", "account_id", "crm_opportunity.account_id","account_id", true);
@@ -11,9 +15,9 @@ jQuery(document).ready(function(){
 	jQuery("#productContainer").remove();
 
 	// 修改部分文字
-	jQuery("#prod_tb .TableHeader").text("服务明细");
-	jQuery("#prod_tb tr:nth-child(2) td:nth-child(2) .TableContent").text("服务编码");
-	jQuery("#prod_tb tr:nth-child(2) td:nth-child(3) .TableContent").text("服务名称");
-	jQuery("#prod_tb tr:nth-child(2) td:nth-child(4) .TableContent").text("收费标准");
-	jQuery("#addProduct").val("添加服务");
+	// jQuery("#prod_tb .TableHeader").text("服务明细");
+	// jQuery("#prod_tb tr:nth-child(2) td:nth-child(2) .TableContent").text("服务编码");
+	// jQuery("#prod_tb tr:nth-child(2) td:nth-child(3) .TableContent").text("服务名称");
+	// jQuery("#prod_tb tr:nth-child(2) td:nth-child(4) .TableContent").text("收费标准");
+	// jQuery("#addProduct").val("添加服务");
 });
